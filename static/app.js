a1e28045cf64be37e7ca6481ed75696c3eb9087f
Steve Dowling <steve@dowling.co.nz>
2021-08-17 21:03:47 +0800

    Removed jQuery and changed dashboard to use InfluxDB instead of the OpenWeatherMap API

--- static/app.js
+++ static/app.js
@@ -1,21 +1,54 @@
 const beerFridgeMethods = {
-    initEventHandlers: function() {
-        $('#setTemp').on('change', function() { beerFridgeMethods.updateSetTemp($(this).val()); });
+    influxBaseUrl: "http://10.1.1.227:8086",
+    influxDatabasseName: "fridgeDB",
+    influxReadMethod: function() { return `${influxBaseUrl}/read?db=${influxDatabasseName}` },
+    influxWrite: function(measurement, field, value) {
+        fetch(`${this.influxBaseUrl}/write?db=${beerFridgeMethods.influxDatabasseName}`, {
+                method: "POST",
+                headers: {'Content-Type': 'application/text'},
+                body: `${measurement} ${field}=${value}`
+            })
+            .catch(console.error) // TODO: Better error handling
     },
-    updateSetTemp: function(val) {
-        $('#setTempValue').text(val);
+    influxReadLast: function(measurement, fields, callback) {
+        fetch(`${this.influxBaseUrl}/query?db=${beerFridgeMethods.influxDatabasseName}&q=SELECT ${fields} FROM ${measurement} ORDER BY time DESC LIMIT 1`)
+            .then(response => response.json())
+            .then(data => {
+                callback(data)
+            })
+            .catch(console.error) // TODO: Better error handling
+    },
+    initEventHandlers: function() {
+        document.getElementById('setTemp').addEventListener('change', event => {
+            var newTemp = event.target.value
+            document.getElementById('setTempValue').textContent = newTemp
+            beerFridgeMethods.influxWrite('SetTemp', 'Temperature', newTemp)
+        })
+    },
+    updateSetTemp: function(newTemp) {
+        document.getElementById('setTempValue').textContent = newTemp
+        beerFridgeMethods.influxWrite('SetTemp', 'Temperature', newTemp)
+    },
+    updateDisplayedSetTemp(newTemp) {
+        document.getElementById('setTemp').value = newTemp
+        document.getElementById('setTempValue').textContent = newTemp        
     },
     updateForecast: function() {
-        $.getJSON( weatherApiUrl, function( data ) { // TODO: Read last value from influx instead of the weather api
-            $( "#currentTemp" ).text( data.current.temp );
+        beerFridgeMethods.influxReadLast('currentTemp', 'temperature,icon,description', function(data) {
+            var fields = data.results[0].series[0].values[0]
+            document.getElementById('currentTemp').textContent = fields[1].toFixed(1)
 
-            var currentWeatherIconUrl = 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png';
-            $('#currentWeatherIcon').attr("src",currentWeatherIconUrl).show();
-          });
+            var currentWeatherIconUrl = `http://openweathermap.org/img/wn/${fields[2]}@2x.png`
+            var currentWeatherIcon = document.getElementById('currentWeatherIcon')
+            currentWeatherIcon.setAttribute('src', currentWeatherIconUrl)
+            currentWeatherIcon.style.display = "inline-block"
+            currentWeatherIcon.title = fields[3]
+        })
     }
 }
 
-$(function() {
-    beerFridgeMethods.updateForecast();
-    beerFridgeMethods.initEventHandlers();
-});
+document.addEventListener('DOMContentLoaded', () => {
+    beerFridgeMethods.updateForecast()
+    beerFridgeMethods.initEventHandlers()
+    beerFridgeMethods.influxReadLast('SetTemp','Temperature', function(data) { beerFridgeMethods.updateDisplayedSetTemp(data.results[0].series[0].values[0][1]) })
+})
